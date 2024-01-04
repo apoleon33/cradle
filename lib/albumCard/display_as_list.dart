d3a5429d8d42eba591ec0da0e52806fc25ed19c3
apoleon33 <simondumesny@gmail.com>
2024-01-04 14:51:26 +0100

    import cleaning

--- lib/albumCard/display_as_list.dart
+++ lib/albumCard/display_as_list.dart
@@ -1,7 +1,5 @@
 import 'package:cradle/albumCard/display_album.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
 import '../album.dart';
 
 class DisplayAsList extends DisplayAlbum {
