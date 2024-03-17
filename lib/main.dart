a29d41c98015cfccc9c724770a50ef9ec7c237df
apoleon33 <simondumesny@gmail.com>
2024-03-17 15:38:10 +0100

    commented again notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
-import 'package:cradle/push_notifications.dart';
 import 'package:cradle/widgets/theme_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:timezone/timezone.dart' as tz;
-import 'package:timezone/data/latest.dart' as tz;
-
 import 'route/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //initNotifications();
-  NotificationService().initNotification();
+  //NotificationService().initNotification();
   runApp(const MyApp());
 }
 
@@ -19,27 +15,27 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
 
-  /// schedule push notifications for a week.
-  void schedulePushNotifications() async {
-    NotificationManagement notificationManagement = NotificationManagement();
-
-
-    tz.initializeTimeZones();
-    tz.TZDateTime birthline = tz.TZDateTime.now(tz.local);
-    tz.TZDateTime deadline = birthline.add(const Duration(days: 7));
-    tz.TZDateTime indexDate = birthline;
-
-    while (deadline.isAfter(indexDate)) {
-      // notificationManagement.removePushNotification(indexDate);
-      bool isNotificationScheduled = await notificationManagement
-          .isNotificationScheduledForThisDay(indexDate);
-
-      if (!isNotificationScheduled) {
-        notificationManagement.addPushNotification(indexDate);
-      }
-      indexDate = indexDate.add(const Duration(days: 1));
-    }
-  }
+  // schedule push notifications for a week.
+  // void schedulePushNotifications() async {
+  //   NotificationManagement notificationManagement = NotificationManagement();
+  //
+  //
+  //   tz.initializeTimeZones();
+  //   tz.TZDateTime birthline = tz.TZDateTime.now(tz.local);
+  //   tz.TZDateTime deadline = birthline.add(const Duration(days: 7));
+  //   tz.TZDateTime indexDate = birthline;
+  //
+  //   while (deadline.isAfter(indexDate)) {
+  //     // notificationManagement.removePushNotification(indexDate);
+  //     bool isNotificationScheduled = await notificationManagement
+  //         .isNotificationScheduledForThisDay(indexDate);
+  //
+  //     if (!isNotificationScheduled) {
+  //       notificationManagement.addPushNotification(indexDate);
+  //     }
+  //     indexDate = indexDate.add(const Duration(days: 1));
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
