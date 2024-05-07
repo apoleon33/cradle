3bb4ead62ac8d6a6b50d401217b65bd3f7fd9b48
apoleon33 <simondumesny@gmail.com>
2024-05-07 15:30:33 +0200

    removed moreINfoMenu widget as it's gonna be reworked

--- lib/widgets/moreInfoMenu.dart
+++ lib/widgets/moreInfoMenu.dart
@@ -39,14 +39,8 @@ class MoreInfoMenu extends StatelessWidget {
 
   Widget builder(
       BuildContext context, MenuController controller, Widget? child) {
-    return IconButton.filled(
-        onPressed: () {
-          if (controller.isOpen) {
-            controller.close();
-          } else {
-            controller.open();
-          }
-        },
-        icon: const Icon(Icons.more_vert));
+    return const IconButton.filled(
+        onPressed: null,
+        icon: Icon(Icons.more_vert));
   }
 }
